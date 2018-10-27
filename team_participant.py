f7e9929e94af3d8d6216886c1bf5e12a9e0cffef
MarkGross <mag210@case.edu>
2018-10-27 15:07:43 -0400

    added __team_roster_url function

--- team_participant.py
+++ team_participant.py
@@ -4,4 +4,13 @@ from bs4 import BeautifulSoup
 #Class for fetching information and statistics about players and coaches
 class team_participant:
 
-    
+    # Produces the expected url for the baseball team roster for the given year
+    # Example Format as follows: https://athletics.case.edu/sports/bsb/2017-18/roster?view=list
+    # NOTE: Does not check that the url is valid or the page exists
+    def __team_roster_url(year):
+        url_beginning = 'https://athletics.case.edu/sports/bsb/'
+        previous_year_string = str(year - 1)
+        dash = '-'
+        current_year_last_two_digits_string = str(year)[-2:]
+        url_end = '/roster?view=list'
+        return (url_beginning + previous_year_string + dash + current_year_last_two_digits_string + url_end)
