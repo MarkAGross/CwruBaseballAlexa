8456665fd39d4d304fb93681fe2f59a00412d1ac
MarkGross <mag210@case.edu>
2018-10-27 15:15:48 -0400

    added __team_individual_statistics_url function

--- team_participant.py
+++ team_participant.py
@@ -14,3 +14,14 @@ class team_participant:
         current_year_last_two_digits_string = str(year)[-2:]
         url_end = '/roster?view=list'
         return (url_beginning + previous_year_string + dash + current_year_last_two_digits_string + url_end)
+
+    # Produces the expected url for the baseball individual statistics page for the given a year
+    # Example Format as follows: https://athletics.case.edu/sports/bsb/2017-18/teams/casewesternreserve?view=lineup&r=0&pos=
+    # NOTE: Does not check that the url is valid or the page exists
+    def __team_individual_statistics_url(year):
+        url_beginning = 'https://athletics.case.edu/sports/bsb/'
+        previous_year_string = str(year - 1)
+        dash = '-'
+        current_year_last_two_digits_string = str(year)[-2:]
+        url_end = '/teams/casewesternreserve?view=lineup&r=0&pos='
+        return (url_beginning + previous_year_string + dash + current_year_last_two_digits_string + url_end)
