4d7e5cce7e52f63dcb510dd8c51c41ad64c018a8
MarkGross <mag210@case.edu>
2018-10-27 14:57:26 -0400

    added functionality to all fetch functions

--- team.py
+++ team.py
@@ -31,55 +31,82 @@ class team:
             __team_stats_dictionary[tr.find_all('td')[0].text.strip()] = tr.find_all('td')[1].text.strip
 
     def fetch_num_of_games:
+        return __team_stats_dictionary["Games"]
 
     def fetch_num_of_at_bats:
+        return __team_stats_dictionary["At Bats"]
 
     def fetch_num_of_runs:
+        return __team_stats_dictionary["Runs"]
 
     def fetch_num_of_hits:
+        return __team_stats_dictionary["Hits"]
 
     def fetch_num_of_doubles:
+        return __team_stats_dictionary["Doubles"]
 
     def fetch_num_of_triples:
+        return __team_stats_dictionary["Triples"]
 
     def fetch_num_of_home_runs:
+        return __team_stats_dictionary["Home Runs"]
 
     def fetch_num_of_runs_batted_in:
+        return __team_stats_dictionary["Runs Batted In"]
 
     def fetch_num_of_extra_base_hits:
+        return __team_stats_dictionary["Extra Base Hits"]
 
     def fetch_num_of_total_bases:
+        return __team_stats_dictionary["Total Bases"]
 
     def fetch_num_of_walks:
+        return __team_stats_dictionary["Walks"]
 
     def fetch_num_of_hit_by_pitches:
+        return __team_stats_dictionary["Hit by pitch"]
 
     def fetch_num_of_strikeouts:
+        return __team_stats_dictionary["Strikeouts"]
 
     def fetch_num_of_sacrifice_flies:
+        return __team_stats_dictionary["Sacrifice Flies"]
 
     def fetch_num_of_sacrifice_hits:
+        return __team_stats_dictionary["Sacrifice Hits"]
 
     def fetch_num_of_hit_into_double_play:
+        return __team_stats_dictionary["Hit into double play"]
 
     def fetch_num_of_stolen_bases:
+        return __team_stats_dictionary["Stolen Bases"]
 
     def fetch_num_of_caught_stealing:
+        return __team_stats_dictionary["Caught Stealing"]
 
     def fetch_batting_average:
+        return __team_stats_dictionary["Batting Average"]
 
     def fetch_on_base_percentage:
+        return __team_stats_dictionary["On Base Percentage"]
 
     def fetch_slugging_percentage:
+        return __team_stats_dictionary["Slugging Percentage"]
 
     def fetch_earned_run_average:
+        return __team_stats_dictionary["Earned Run Average"]
 
     def fetch_num_of_shutouts:
+        return __team_stats_dictionary["Shutouts"]
 
     def fetch_num_of_at_bats_against:
+        return __team_stats_dictionary["At Bats Against"]
 
     def fetch_batting_average_against:
+        return __team_stats_dictionary["Batting Average Against"]
 
     def fetch_home_attendance:
+        return __team_stats_dictionary["Home Attendance"]
 
     def fetch_home_attendance_average:
+        return __team_stats_dictionary["Home Attendance average"]
