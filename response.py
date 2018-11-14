1983c845afb66fa2b130b6cf709bb14e3837a59e
Steven Barker <sbarker1839@gmail.com>
2018-11-14 16:49:38 -0500

    Fixed counter in response fucntion
    Reponse now works for three test inputs, one player stat, one team stat, and an error

--- response.py
+++ response.py
@@ -12,17 +12,18 @@ class response(object):
     team = []
 
     testTeamStat = ["RBIs", "52", "2018"]
-    testPlayerStat = ["12", "RBI", "17", "2018"]
+    testPlayerStat = ["12", "RBIs", "17", "2018"]
+    testError = ["x"]
 
-    def response(keyList, *args):
+    def response(keyList):
         statString = ""
         playerString = ""
         statNumString = ""
         yearString = ""
-        count = 0
+        
 
-        for num in args:
-            count += count
+        count = len(keyList)
+            
         print(count)
 
         if(count == 3):
@@ -41,8 +42,12 @@ class response(object):
               statNumString = keyList[2] 
               yearString = keyList[3]
 
-        print(playerString+ " has " + statNumString + " " + statString + " in " + yearString)
+          print("Player No." +playerString+ " has " + statNumString + " " + statString + " in " + yearString)
+
+        elif(count <= 2):
+          print("Sorry I did not retrieve enough infomation to answer your question")
 
 
     response(testTeamStat)
     response(testPlayerStat)
+    response(testError)
