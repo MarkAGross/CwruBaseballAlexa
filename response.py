49c58078812414ebeeeed54236118ea6e38debbf
Steven Barker <sbarker1839@gmail.com>
2018-11-18 21:16:45 -0500

    Update response.py

--- response.py
+++ response.py
@@ -1,53 +1,138 @@
 
-
+from error import LACK_OF_TEAM_INFORMATION_ERROR
+from error import LACK_OF_PARTICIPANT_INFORMATION_ERROR
 class response(object):
 
     #create mock list of player info
     #creat func that takes list and outputs sentance
 
-    teamGamesPlayedCurr = ["The CWRU Baseball team has played" , "games this year"]
-    teamGamesPlayed = ["The CWRU Baseball team played" , "in"]
-    teamAtBatsCurr = []
-    teamAtBats = []
-    team = []
+   
+    
 
-    testTeamStat = ["RBIs", "52", "2018"]
-    testPlayerStat = ["12", "RBIs", "17", "2018"]
-    testError = ["x"]
+    testTeamStat = [None] * 27
+    testTeamStat[2] = "15"
+    testTeamStat[26] = "2018"
+    testPlayerStat = [None] * 33
+    testPlayerStat[5] = "SR."
+    testPlayerStat[32] = "2017"
 
-    def response(keyList):
+
+    #function to format and return information about team stats
+    def teamResponse(keyList):
+        teamStatList = ["Games","At Bats", "Runs", "Hits", "Doubles", "Triples", "Home runs"
+                    "RBIs", "Extra Base Hits", "Total Bases", "Walks", "Hit by pitches",
+                    "Strikeouts", "Sacrifice flies", "Sacrifice hits", "Hits into double play",
+                    "Stolen bases", "Caught stealing", "Batting average", "On base percentage",
+                    "Slugging percentage", "ERA", "Shutouts", "At bats against",
+                    "Batting average against", "Home attendance", "Home attendance average", "Year"]
+        count = 0
+        statArray = []
+        indexList = []
+        nameStatArray = []
         statString = ""
-        playerString = ""
         statNumString = ""
         yearString = ""
+
+       # try:
+        for i in range(len(keyList)):
+            if keyList[i] is not None:
+                statArray.append(keyList[i])
+                indexList.append(i)
+                count +=1
+
+        for i in range(len(indexList)):
+            nameStatArray.append(teamStatList[indexList[i]])
+        
+        statString = nameStatArray[0]
+        statNumString = statArray[0]
+        #yearString = statArray[1]
+
+        print("The CWRU Baseball team has "+ statNumString +" "+ statString + " in " + yearString)
+
+       # except count<=1:
+            #raise LACK_OF_TEAM_INFORMATION_ERROR
+
+        print(statArray)
+        print(indexList)
+        print(nameStatArray)
+
+    #function to format and return information about individual participant stats
+    def participantResponse(keyList):
+        participantStatList = ["Name", "Position", "Bats and throws", "Height", "Weight", "Academic Year",
+                           "Hometown and high school", "Games played", "At bats", "Runs", "Hits",
+                           "Doubles", "Triples", "Home runs", "RBIs", "Walks", "Strikeouts",
+                           "Stolen Bases", "Batting Average", "On base percentage", "Slugging percentage",
+                           "Appearances", "Game starts", "Wins", "Losses", "Saves", "Complete games",
+                           "Innings pitched", "Strikeouts per 9 innings", "Earned runs", "ERA", "Player number", "Year"]
+        count = 0
+        statArray = []
+        indexList = []
+        nameStatArray = []
+        statString = ""
+        statNumString = ""
+        playerString = ""
+        yearString = ""
+
+        try:
+            for i in range(len(keyList)):
+                if keyList[i] is not None:
+                    statArray.append(keyList[i])
+                    indexList.append(i)
+                    count +=1
+
+            for i in range(len(indexList)):
+                nameStatArray.append(participantStatList[indexList[i]])
+
+            statString = nameStatArray[0]
+            statNumString = statArray[0]
+            yearString = statArray[1]
+
+            print("Player No." +playerString+ " has " + statNumString + " " + statString + " in " + yearString)
+        
+        except count<=2:
+            raise LACK_OF_PARTICIPANT_INFORMATION_ERROR
+            
+        print(statArray)
+        print(indexList)
+        print(nameStatArray)
+
+    #function to format and return information about team schedule
+    def scheduleResponse(keyList):
+        scheduleStatList = ["Next game", "Previous game"]
+        count = 0
+        statArray = []
+        indexList = []
+        nameStatArray = []
+        statString = ""
+        statNumString = ""
+        playerString = ""
+        yearString = ""
+
+        for i in range(len(keyList)):
+            if keyList[i] is not None:
+                statArray.append(keyList[i])
+                indexList.append(i)
+                count +=1
+
+        for i in range(len(indexList)):
+            nameStatArray.append(scheduleStatList[indexList[i]])
+
+        statString = nameStatArray[0]
+        statNumString = statArray[0]
+        yearString = "2018"
+
+        print()
+        print(statArray)
+        print(indexList)
+        print(nameStatArray)
         
 
-        count = len(keyList)
-            
-        print(count)
-
-        if(count == 3):
-            
-            for x in range(len(keyList)):
-              statString = keyList[0] 
-              statNumString = keyList[1] 
-              yearString = keyList[2]
-
-            print("The CWRU Baseball team has "+ statNumString +" "+ statString + " in " + yearString)
-
-        elif(count == 4):
-          for x in range(len(keyList)):
-              playerString = keyList[0]
-              statString = keyList[1] 
-              statNumString = keyList[2] 
-              yearString = keyList[3]
-
-          print("Player No." +playerString+ " has " + statNumString + " " + statString + " in " + yearString)
-
-        elif(count <= 2):
-          print("Sorry I did not retrieve enough infomation to answer your question")
 
 
-    response(testTeamStat)
-    response(testPlayerStat)
-    response(testError)
+
+
+    #response(testTeamStat)
+    #response(testPlayerStat)
+    #response(testError)
+    teamResponse(testTeamStat)
+    participantResponse(testPlayerStat)
