7cfa346c3a63659dd34739427d72c2af2bd68fef
Steven Barker <sbarker1839@gmail.com>
2018-11-18 21:44:04 -0500

    Added imports to demo class

--- demo.py
+++ demo.py
@@ -3,6 +3,8 @@ from team_participant import team_participant
 from schedule import schedule
 from RequestReciever import receiver
 from response import response
+from bs4 import BeautifulSoup
+import urllib.request
 import sys
 
 inputstring = "how many games has number 29 played in 2018"
